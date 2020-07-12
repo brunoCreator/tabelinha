f31e99e1f61be6891c719a11e854f504c5942ae5
brunoCreator <brunoCreator@users.noreply.github.com>
2020-07-12 18:51:28 -0300

    opacity

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-window.addEventListener("load", ativar());
+/*window.addEventListener("load", ativar());
 
 function ativar(){
     var path = window.location.href.split("/").pop();
     var els = document.querySelectorAll("a[href='"+path+"']");
     els[0].classList.add("active");
     els[1].classList.add("active");
+}*/
+var steam = document.getElementById("steam");
+var xbox = document.getElementById("xbox");
+var playstation = document.getElementById("playstation");
+
+steam.addEventListener('mouseover', opaco);
+xbox.addEventListener('mouseover', opaco);
+playstation.addEventListener('mouseover', opaco);
+
+steam.addEventListener('mouseout', normal);
+xbox.addEventListener('mouseout', normal);
+playstation.addEventListener('mouseout', normal);
+
+function opaco(){
+    this.style.opacity = "0.5";
+}
+
+function normal(){
+    this.style.opacity = "1";
 }
